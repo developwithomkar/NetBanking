b9d3f50997356459eac0035420ebe6d526c45cbe
Omkar Prabhudesai <developwithomkar@outlook.com>
2019-09-22 23:11:49 +0530

    IoC.DependencyContainer - implementation done

---
+++ NetBanking.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetBanking.Domain.Core.Events;
+using NetBanking.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetBanking.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain.Bus == RabbitMQ
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
